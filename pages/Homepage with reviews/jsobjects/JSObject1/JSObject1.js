1c589f2bef44a744cb449c6d71f48c047d7f7dd8
Ananya Shah <anyushah@gmail.com>
2023-04-24 08:01:52 +0000

    created

---
+++ pages/Homepage with reviews/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,7 @@
+export default {
+
+	myFun1 () {
+		{{GetWorkplaces.run()}}
+	},
+	
+}
---
+++ pages/Sign up/jsobjects/creating_user/creating_user.js
@@ -0,0 +1,9 @@
+export default {
+  myFun1 () {
+    return {
+      username: "{{Username_Signin.inputText}}",
+      email: "{{Email_Signin.inputText}}",
+      password: "{{Password_Signin.inputText}}"
+    }
+  }
+}
